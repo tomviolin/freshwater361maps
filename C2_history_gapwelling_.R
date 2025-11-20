fb826f9901e7d060de9900e1bfe90ce50b80b63e
Tom Hansen <tomh@uwm.edu>
2025-11-20 11:21:30 -0600

    upwelling map

--- C2_history_gapwelling_.R
+++ C2_history_gapwelling_.R
@@ -9,7 +9,6 @@ loadlibrary(data.table)
 
 
 
-pdf("shipgap.pdf",width = 11,height=8.5,family = "Helvetica")  # also jpeg() png()  pdf() 
 cpal = rev(RColorBrewer::brewer.pal(11,"Spectral"))[c(1,2,3,4,5,6,7,8,9,10,11)]
 cpal = colorRampPalette(cpal)(1000)
 
@@ -22,11 +21,12 @@ hsvpal[2,] = .9
 
 #hsvpal[1,] = seq(0,0.7,length.out=NC)
 cpal = hsv(hsvpal[1,],hsvpal[2,],hsvpal[3,])
-alldata = fread("fixedhistory.csv")# Read in the ship data
+alldata = fread("fixedhistory.csv.gz")# Read in the ship data
 gap_kml = sf::st_read("gap_area.kml")
-bb = as.data.frame(st_bbox(gap_kml$geometry))
 
-bbox_data = bb$x
+bb = st_bbox(gap_kml$geometry)
+
+bbox_data = bb
 
 data = alldata[
   (alldata$GPS_Lat >= bbox_data[2]) &
@@ -39,14 +39,16 @@ data$recdate = as.POSIXct(data$recdate)
 
 # Establish the bounding box (the coordinates of a rectangle that defines the map)  +/- 0.001 for margin
 map_bbox = sf::st_bbox(c(
-  xmin=min(data$GPS_Long)-0.01,
-  ymin=min(data$GPS_Lat)-0.01,
-  xmax=max(data$GPS_Long)+0.01,
-  ymax=max(data$GPS_Lat)+0.01),crs=4623)
+  xmin=min(data$GPS_Long)  -0.001,
+  ymin=min(data$GPS_Lat)   -0.001,
+  xmax=max(data$GPS_Long)  +0.001,
+  ymax=max(data$GPS_Lat)   +0.001
+  ),crs=4623)
 
+#map_bbox = bb
 
 #get tiles
-tiles_map <- get_tiles(x = map_bbox, provider = "OpenStreetMap.DE",zoom=13, crop=TRUE)
+tiles_map <- get_tiles(x = map_bbox, provider = "OpenStreetMap.DE",zoom=15, crop=TRUE)
 
 
 temps = data$Temp_C         # temps is in range min to max
@@ -55,9 +57,25 @@ temps = temps / max(temps)  # now temps is in range 0 to 1
 
 trackcolors = cpal[floor(temps*NC*0.999+1)]
 #plot(data$GPS_Long,data$GPS_Lat,cex=.2,col=trackcolors, main="Ship Data 9/6/2025",asp=1) # ship track
+
+pdf("shipgap.pdf",width = 11,height=8.5,family = "Helvetica")  # also jpeg() png()  pdf() 
+
+
 plot_tiles(tiles_map, add = FALSE) # Plot the tiles
-points(data$GPS_Long,data$GPS_Lat,cex=.3,col=rgb(0,0,1,0.5), main="Ship Data 9/6/2025",asp=1) # ship track
+#points(data$GPS_Long,data$GPS_Lat,cex=.3,col=rgb(0,0,1,0.5), main="Ship Data 9/6/2025",asp=1) # ship track
 points(data$GPS_Long,data$GPS_Lat,cex=.2,col=trackcolors, main="Ship Data 9/6/2025",asp=1) # ship track
+
+
+points(range(data$GPS_Long),range(data$GPS_Lat),cex=2,col='red')
+
+# lines(
+#   map_bbox[c(1,3)],
+#   map_bbox[c(2,4)],
+#   type='l',
+#   col='red')
+
+
+
 legendplaces = rep(NULL,NC)
 leglabels = seq(
   round(min(data$Temp_C),1),
@@ -76,11 +94,6 @@ legendbbox = c(
   ymax=map_bbox[4]
 )
 
-lines(
-  map_bbox[c(1,3)],
-  map_bbox[c(2,4)],
-  type='l',
-  col='red')
 
 
 
@@ -88,7 +101,7 @@ color.legend(yt=quantile(legendbbox[c(2,4)],0.99),
              yb=quantile(legendbbox[c(2,4)],0.55),
              xl=quantile(legendbbox[c(1,3)],0.975),
              xr=quantile(legendbbox[c(1,3)],0.99),
-             cex=0.1,
+             cex=0.8,
              legend=legendplaces,
              col="black", #cpal,
              rect.col=cpal, gradient = "y")
@@ -99,7 +112,7 @@ print(map_bbox[c(2,4)])
 
 
 dev.off() # only if saving to file
-system2("open","shipmap.pdf")
+system2("open","shipgap.pdf")
 # get_credit("Esri.NatGeoWorldMap")
 # [1] "Tiles © Esri - National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
 ##sf_polygon <- getbb("Milwaukee, WI", format_out = "sf_polygon") # this is useful!
